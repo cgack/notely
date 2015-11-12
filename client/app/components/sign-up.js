a6bb80e3a609227905a18690b90681afd424afd6
Cory Gackenheimer <cory.gack@gmail.com>
2015-11-12 13:01:28 -0500

    moar signup work

--- client/app/components/sign-up.js
+++ client/app/components/sign-up.js
@@ -1,6 +1,20 @@
 angular.module('notely')
-.directive('signUp', function() {
+.directive('signUp', () => {
+
+  class SignUpController {
+    constructor() {
+      this.user = {};
+    }
+
+    submit() {
+      alert(this.user.username);
+    }
+  }
   return {
+      scope: {},
+      controller: SignUpController,
+      controllerAs: 'ctrl',
+      bindToController: true,
       templateUrl: '/components/sign-up.html'
   };
 });
