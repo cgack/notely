5cff8b08b6b276fb9acfaa341bacc23c32f9a381
Cory Gackenheimer <cory.gack@gmail.com>
2015-11-12 10:19:18 -0500

    more v1 api endpoint plus gulpfile

---
+++ server/routes/notes.js
@@ -0,0 +1,60 @@
+var router = require('express').Router();
+var Note = require('../models/note');
+
+router.get('/', function(req, res) {
+  Note.find().sort({ updated_at: -1 })
+  .then(function(notes){
+      res.json(notes);
+  });
+});
+
+router.post('/', function(req, res) {
+  var note = new Note({
+      title: req.body.note.title,
+      body_html: req.body.note.body_html
+  });
+
+  note.save().then(function(noteData) {
+    res.json({
+      message: "saved!",
+      note: noteData
+    });
+  });
+});
+
+router.put('/:id', function(req, res) {
+  Note.findOne({ _id: req.params.id })
+  .then(function(note) {
+      note.title = req.body.note.title;
+      note.body_html = req.body.note.body_html;
+
+      note.save().then(function(noteData) {
+        res.json({
+          message: "changes saved!",
+          note: noteData
+        });
+      });
+  }, function(err) {
+    res.json({
+      message: 'an error occured',
+      error: err
+    });
+  });
+});
+
+router.delete('/:id', function(req, res) {
+  Note.findOne({ _id: req.params.id })
+  .then(function(note) {
+      note.remove().then(function(){
+        res.json({
+          message: 'note removed',
+          note: note
+        });
+      });
+  }, function(err) {
+
+  });
+});
+
+
+module.exports = router;
