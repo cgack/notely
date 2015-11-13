323c098590c790124dc3124970a59c31f8964a9a
Cory Gackenheimer <cory.gack@gmail.com>
2015-11-13 15:35:00 -0500

    html5 mode

--- client/app/app.js
+++ client/app/app.js
@@ -5,11 +5,13 @@
     'flash'
   ]);
 
-  function config($urlRouterProvider) {
+  function config($urlRouterProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
     $urlRouterProvider.otherwise('/notes/');
+
   }
 
-  config['$inject'] = ['$urlRouterProvider'];
+  config['$inject'] = ['$urlRouterProvider', '$locationProvider'];
   app.config(config);
   app.constant('API_BASE', '//localhost:3000/api/v1/');
 })();
